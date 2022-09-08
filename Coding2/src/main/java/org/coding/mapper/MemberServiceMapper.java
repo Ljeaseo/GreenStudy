310e4b98d06853b1f529aa364622423b781277e8
Ljeaseo <ljseo0111@naver.com>
2022-09-08 15:27:44 +0900

    mergetest

--- Coding2/src/main/java/org/coding/mapper/MemberServiceMapper.java
+++
@@ -1,9 +0,0 @@
-package org.coding.mapper;
-
-import org.coding.model.MemberVo;
-
-public interface MemberServiceMapper {
-	public void memberjoin(MemberVo member);
-	
-	public MemberVo login(MemberVo member);
-}
